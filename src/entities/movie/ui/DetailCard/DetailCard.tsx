85346863ea54b88efe0320089708defec54ee457
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-03-22 17:51:49 +0300

    refactor: extract detail card styles

--- src/entities/movie/ui/DetailCard/DetailCard.tsx
+++ src/entities/movie/ui/DetailCard/DetailCard.tsx
@@ -4,16 +4,20 @@ import { useUnit } from "effector-react";
 import { useParams } from "react-router";
 import { MovieCardImage } from "../MovieImage/MovieImage";
 import { DetailCardText } from "../DetailCardText/DetailCardText";
+import { $favoritesMovies } from "../../../../features/toggle-favorite/model/favorites.store";
+import styles from "./DetailCard.module.css";
 
 export function DetailCard() {
   const { id } = useParams();
-  const [movies] = useUnit([$movies]);
-  const currentMovie = movies.find((movie) => movie.id.toString() === id);
+  const [movies, favoritesMovies] = useUnit([$movies, $favoritesMovies]);
+  const currentMovie =
+    movies.find((movie) => movie.id.toString() === id) ??
+    favoritesMovies.find((movie) => movie.id.toString() === id);
   if (!currentMovie) return <Paragraph>Этот фильм не найден!</Paragraph>;
   return (
     <Box padding="2xl">
-      <Card style={{ display: "grid", gridTemplateColumns: "30% 70%", minHeight: "500px" }}>
-        <Box padding="2xl" height="95%">
+      <Card className={styles["detail-card"]}>
+        <Box padding="2xl">
           <MovieCardImage name={currentMovie.name} poster={currentMovie.poster} />
         </Box>
         <DetailCardText {...currentMovie} />
