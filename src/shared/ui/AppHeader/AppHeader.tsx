5052ec6e6ef5dc7800da6d7a5fbc0c7ebfb3fcc2
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-03-22 19:47:30 +0300

    feat: adjust header styles

--- src/shared/ui/AppHeader/AppHeader.tsx
+++ src/shared/ui/AppHeader/AppHeader.tsx
@@ -9,7 +9,7 @@ export function AppHeader() {
       <Box padding="l" className={styles["header-wrapper"]}>
         <div className={styles.logo}>
           <Link href={ROUTES.MAIN}>
-            <Headline>KinoVed</Headline>
+            <Headline level="1">KinoVed</Headline>
           </Link>
         </div>
         <NavButtons />
